4554651a87cf58086ada8fdd44fa960f10181ec3
Kaityo1221 <rakugo100@gmail.com>
2026-05-07 22:09:45 +0900

    storage.js を作成
    ・storage.jsを新規作成<br> ・ローカル保存用スクリプトを追加<br> ・初期読み込み確認用ログを追加<br>

---
+++ js/storage.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*
+  GET - Global EN Trainer
+  Storage Script
+  ローカル保存用
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Storage system ready");
+});
