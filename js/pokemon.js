e1faf1c624a0dbb36a9e90fe5be222028b30d4a9
Kaityo1221 <rakugo100@gmail.com>
2026-05-07 22:07:39 +0900

    pokemon.js を作成
    ・pokemon.jsを新規作成<br> ・ポケモン名データバンク用スクリプトを追加<br> ・初期読み込み確認用ログを追加<br>

---
+++ js/pokemon.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*
+  GET - Global EN Trainer
+  Pokémon Name Bank Script
+  ポケモン名データバンク用
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Pokemon Name Bank ready");
+});
