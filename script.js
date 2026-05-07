b151246de3cbc6e08b3ffdbb024239980c338bc4
Kaityo1221 <rakugo100@gmail.com>
2026-05-07 22:44:46 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 "use strict";
 
 /*
+  ====================================
   GET - Global EN Trainer
   Main Script
-  初期共通処理用
+  ====================================
 */
 
 document.addEventListener("DOMContentLoaded", () => {
+
   console.log("GET initialized");
+
 });
