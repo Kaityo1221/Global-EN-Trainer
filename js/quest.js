2be04d5d38e6bf82476df9f62522bf146b37195e
Kaityo1221 <rakugo100@gmail.com>
2026-05-29 06:21:57 +0900

    quest.js を更新

--- js/quest.js
+++ js/quest.js
@@ -9,3 +9,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Quest system ready");
 });
+document.addEventListener("DOMContentLoaded", () => {
+
+  const bgm =
+    document.getElementById("enquestBgm");
+
+  if(!bgm){
+    return;
+  }
+
+  bgm.volume = 0.35;
+
+  document.body.addEventListener("touchstart", () => {
+    bgm.play();
+  }, { once:true });
+
+  document.body.addEventListener("click", () => {
+    bgm.play();
+  }, { once:true });
+
+});
